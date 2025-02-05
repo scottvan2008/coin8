fe4df02a6c0ac6996c686bb6c54b3bf7b5bd4881
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-05 16:02:34 +0800

    mostly work

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Link from 'next/link'
 import { useTranslation } from '../i18n'
+import { Footer } from "./components/Footer";
 
-export default function Home({ params: { lng } }) {
+export default async function Home({ params: { lng } }) {
+
+  const { t } = await useTranslation(lng)
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
                 <Image
@@ -16,13 +21,14 @@ export default function Home({ params: { lng } }) {
           priority
         />
         <div>
-        <h1>Hi there!</h1>
+      <h1>{t('title')}</h1>
       <Link href={`/${lng}/second-page`}>
-        second page
+        {t('to-second-page')}
       </Link>
         </div>
 
       </main>
+      <Footer lng={lng} />
       
     </div>
   );
--- app/[lng]/second-page/page.tsx
+++ app/[lng]/second-page/page.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import { useTranslation } from '../../i18n'
+import { Footer } from '../components/Footer'
 
-export default function Page({ params: { lng } }) {
+export default async function Page({ params: { lng } }) {
+
+    const { t } = await useTranslation(lng, 'second-page')
+
   return (
     <>
-      <h1>Hi from second page!</h1>
+<h1>{t('title')}</h1>
       <Link href={`/${lng}`}>
-        back
+        {t('back-to-home')}
       </Link>
+
+      <Footer lng={lng} />
     </>
   )
 }
